7cd7ab7ffcf560932150313043c2066b88218af2
Rodrigo Santiago <33rodrigosantiago@gmail.com>
2020-07-07 17:56:36 -0300

    Adicionado funcionalidades para contar o número de aparições e inicio de um esboço (Grafico de calor), por enquanto em outra página (html) mas quero arrumar o infoProteina.html para reconhecer meu código HTML gerado

--- BetaTCC/Modelagem/views.py
+++ BetaTCC/Modelagem/views.py
@@ -20,7 +20,30 @@ def listaProteina(request):
 def infoProteina(request, id):
     proteina = Proteina.objects.get(pk=id)
 
+    dicionario = {'A':0,'R':0,'N':0,'D':0,'E':0,'C':0,'G':0,'Q':0,'H':0,'I':0,'L':0,'K':0,'M':0,'F':0,'P':0,'S':0,'Y':0,'T':0,'W':0,'V':0}
+    sequencia = proteina.sequencia
+    #count = sequencia.count()
+    for i in sequencia:
+        if i in dicionario:
+            dicionario[i] = dicionario[i] + 1
+    
+    # print(dicionario)
+
+    transparencia = 0,5
+    grafico = "<div>"
+
+    for i in dicionario:
+        if dicionario[i] > 0:
+            transparencia = dicionario[i] / len(dicionario)
+            grafico += "<div style='width:100px; border:1px solid #111; color:#fff; height:100px; float:left; background-color:rgba(0, 0, 0, "+ str(transparencia) +"')>"+ str(dicionario[i]) + "</div>"
+    grafico += "</div>"
+
+    saida = open("bgl.html","w")
+    saida.write(grafico)
+    saida.close()
     return render(request, 'modelagem/infoProteina.html', 
     {
-        'prot': proteina
+        'prot': proteina,
+        'dados': dicionario,
+        'grafico': grafico
     })
