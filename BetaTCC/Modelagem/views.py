5883b5d3df533cacee068976f1a361673457cbb6
Rodrigo Santiago <33rodrigosantiago@gmail.com>
2020-07-08 18:21:51 -0300

    Adicionado um contador na função de infoProteina, tem que arrumar esse calculo estranho ai!

--- BetaTCC/Modelagem/views.py
+++ BetaTCC/Modelagem/views.py
@@ -29,23 +29,30 @@ def infoProteina(request, id):
     
     transparencia = {'A':0,'R':0,'N':0,'D':0,'E':0,'C':0,'G':0,'Q':0,'H':0,'I':0,'L':0,'K':0,'M':0,'F':0,'P':0,'S':0,'Y':0,'T':0,'W':0,'V':0}
 
-
+    
     sequencia = proteina.sequencia
-    #count = sequencia.count()
+    contador = 0
+    
     for i in sequencia:
         if i in dicionario:
             dicionario[i] = dicionario[i] + 1
+            contador = contador + 1
     
     # Tentando criar um div dinâmica para retornar através do Render
     # grafico = "<div>"
 
     for i in dicionario:
         if dicionario[i] > 0:
-            transparencia[i] = dicionario[i] / len(dicionario)
+            transparencia[i] = dicionario[i]  / contador
+            print(transparencia[i])
+
         else: 
             transparencia[i] = 0
 
-    #         grafico += "<div style='width:100px; border:1px solid #111; color:#fff; height:100px; float:left; background-color:rgba(0, 0, 0, "+ str(transparencia) +"')>"+ str(dicionario[i]) + "</div>"
+    #print(len(sequencia.replace('\n','')))
+    print(contador)
+    
+    # grafico += "<div style='width:100px; border:1px solid #111; color:#fff; height:100px; float:left; background-color:rgba(0, 0, 0, "+ str(transparencia) +"')>"+ str(dicionario[i]) + "</div>"
     # grafico += "</div>"
 
     #print(transparencia)
