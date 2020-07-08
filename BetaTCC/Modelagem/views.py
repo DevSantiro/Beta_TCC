ac8fd07adc751a64b07b2417e098a0dc42cd596c
Rodrigo Santiago <33rodrigosantiago@gmail.com>
2020-07-08 15:34:25 -0300

    Realizado ajustes na representação gráfica

--- BetaTCC/Modelagem/views.py
+++ BetaTCC/Modelagem/views.py
@@ -20,30 +20,46 @@ def listaProteina(request):
 def infoProteina(request, id):
     proteina = Proteina.objects.get(pk=id)
 
+    # Todas as possiveis contagens zeradas
     dicionario = {'A':0,'R':0,'N':0,'D':0,'E':0,'C':0,'G':0,'Q':0,'H':0,'I':0,'L':0,'K':0,'M':0,'F':0,'P':0,'S':0,'Y':0,'T':0,'W':0,'V':0}
+    
+    # Aqui será representado a transparência para cada caso
+    # Tentei assim não consegui adaptar em uma lista de dicionario mas quem sabe
+    # transparencia = ({'A':0, 'tranparencia': 55}, {'R':0},{'N':0},{'D':0},{'E':0},{'C':0},{'G':0},{'Q':0},{'H':0},{'I':0},{'L':0},{'K':0},{'M':0},{'F':0},{'P':0},{'S':0},{'Y':0},{'T':0},{'W':0},{'V':0})
+    
+    transparencia = {'A':0,'R':0,'N':0,'D':0,'E':0,'C':0,'G':0,'Q':0,'H':0,'I':0,'L':0,'K':0,'M':0,'F':0,'P':0,'S':0,'Y':0,'T':0,'W':0,'V':0}
+
+
     sequencia = proteina.sequencia
     #count = sequencia.count()
     for i in sequencia:
         if i in dicionario:
             dicionario[i] = dicionario[i] + 1
     
-    # print(dicionario)
-
-    transparencia = 0,5
-    grafico = "<div>"
+    # Tentando criar um div dinâmica para retornar através do Render
+    # grafico = "<div>"
 
     for i in dicionario:
         if dicionario[i] > 0:
-            transparencia = dicionario[i] / len(dicionario)
-            grafico += "<div style='width:100px; border:1px solid #111; color:#fff; height:100px; float:left; background-color:rgba(0, 0, 0, "+ str(transparencia) +"')>"+ str(dicionario[i]) + "</div>"
-    grafico += "</div>"
+            transparencia[i] = dicionario[i] / len(dicionario)
+        else: 
+            transparencia[i] = 0
 
-    saida = open("bgl.html","w")
-    saida.write(grafico)
-    saida.close()
+    #         grafico += "<div style='width:100px; border:1px solid #111; color:#fff; height:100px; float:left; background-color:rgba(0, 0, 0, "+ str(transparencia) +"')>"+ str(dicionario[i]) + "</div>"
+    # grafico += "</div>"
+
+    #print(transparencia)
+
+    # Abri em um arquivo.html somente para visualizar como estão saindo os resultados
+    # saida = open("bgl.html","w")
+    # saida.write(grafico)
+    # saida.close()
+
     return render(request, 'modelagem/infoProteina.html', 
     {
         'prot': proteina,
         'dados': dicionario,
-        'grafico': grafico
+        'transparencia': transparencia
     })
+
+    #return HttpResponse(grafico)
