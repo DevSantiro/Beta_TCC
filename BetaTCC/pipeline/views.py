0dc409093888480290b3ca17ed6c1ce4d3759c2a
Rodrigo Santiago <33rodrigosantiago@gmail.com>
2020-07-24 19:43:59 -0300

    várias funções do pipeline adicionadas, modelagem das Proteinas 50% feito, possui alguns bugs de redirecionamento de tela, possivelmente por causa do FORM, verificar depois!

--- BetaTCC/pipeline/views.py
+++ BetaTCC/pipeline/views.py
@@ -4,7 +4,9 @@ from django.core.files.storage import FileSystemStorage
 from django.core.files import File
 from django.core.files.uploadedfile import UploadedFile
 from django import forms
-
+from Bio.PDB import Polypeptide
+import os
+import subprocess
 
 class UploadFileForm(forms.Form):
     title = forms.CharField(max_length=50)
@@ -25,35 +27,68 @@ def Index(request):
 # Também será necessário configurar o MEDIA_ROOT e MEDIA_URL em settings.py
 def upload(request):
     if request.method == 'POST':
-        uploaded_file = request.FILES['document']
+        
+        proteina = request.FILES['proteina']
+        template = request.FILES['documento']
+
         fs = FileSystemStorage()
         
-        fs.save(uploaded_file.name, uploaded_file)
+        fs.save(proteina.name, proteina)
+        fs.save(template.name, template)
         
-        caminho = fs.open(uploaded_file.name, 'r')
+        # Estou nesse processo do Pipeline! Eu tenho os arquivos e preciso usar eles para continuar no pipeline.
+        # O Primeiro arquivo será o Template do .pdb preciso alterar o "id_maior" para o nome do arquivo que subi como upload.
+        # A variavel "sequencia" neste contexto, será substituida pelo meu arquivo .fasta que será gerado o modelo.
+
+        w = open("media\\"+ template.name +".fasta","w")
+        w.write(">"+template.name+"\n")
+        cadeia = 'A'
+        comeco = 0
+        fim = 0
+        pdb = open('media\\' + template.name).readlines()
+        for linha in pdb:
+            if linha[0:4] == "ATOM" and linha[21] == cadeia and linha[13:15] == 'CA':
+                resname3 = linha[17:20]
+                if comeco == 0:
+                    comeco = int(linha[22:26])
+                if int(linha[22:26]) > fim:
+                    fim = int(linha[22:26])
+                resname1 = Polypeptide.three_to_one(resname3)
+                w.write(resname1)
+        w.write("\n")
+        w.close()
+        os.system("type media\\"+template.name+".fasta > media\\alinha.fasta")
+        os.system("type media\\"+proteina.name+" >> media\\alinha.fasta")
         
-        conteudo = File(caminho)
-        
-        print(conteudo)
+        # run clustal-w
+        os.system("clustalw2 -infile=media\\alinha.fasta -output=pir")
+        #subprocess.call(["clustalw2.exe", "-infile='media\\alinha.fasta' -output='pir'"])
+        aln = open("media\\alinha.pir").readlines()
+        new_aln = open("media\\new_alinha.pir","w")
+        tipo = 0 #0 = PDB; 1 = SEQ
+        seq = open("media\\" + proteina.name)
+        seq_final = ""
+        for linha in seq:
+            if linha[0] != ">":
+                seq_final += linha.strip()
+        tamanho_seq = len(seq_final)
+        print("tamanho da seq = "+str(tamanho_seq))
+        for linha in aln:
+            if linha[0] == ">":
+                if tipo == 0 and linha != "\n":
+                    new_aln.write(">P1;"+template.name+"\n")
+                    new_aln.write("structure:"+template.name+":"+str(comeco)+":"+cadeia+":"+str(fim)+":"+cadeia+"::::\n")
+                    tipo = tipo+1
+                elif tipo == 1:
+                    new_aln.write(">P1;"+proteina.name+"\n")
+                    new_aln.write("sequence:"+proteina.name+":"+str(1)+":A:"+str(tamanho_seq)+":A::::")
+            else:
+                new_aln.write(linha)
+        new_aln.close()
+
+        #os.system("blastp -query media/" + proteina.name + " -subject media/" + multi_fasta.name + " -outfmt=6 > media/resultado.txt")
 
     return render(request, 'pipeline/upload.html')
-
-
-#  Observações sobre esse tipo de implementação:
-#  Aqui eu consegui gerar uma função que recebe o arquivo enviado por 'POST' e pegar o conteúdo do arquivo enviado! 
-#  No entando eu precisei utilizar um formulário (preciso verificar e adaptar melhor o uso do mesmo) UploadFileForm.
-#  Ainda não está da forma que eu quero, mas já é um bom esboço para implementar a ideia do projeto.
-#  É importante lembrar que apesar de conseguir ler o conteúdo, existem vários pequenos problemas, como os caracteres "\r" e "\n" (pesquisar para que serve "\r")
-#  PS: optei por não armazenar os arquivos enviados por upload em um diretório (por enquanto), somente para evitar lixo eletrônico.
-
-# Agora preciso trabalhar melhor com os valores obtidos através de um arquivo disparado direto da página, por "padrão", vou tentar considerar
-# somente arquivos do tipo ".fasta", facilitando o trabalho. 
-# Preciso ignorar o header (1º Linha do arquivo), o mesmo começa com um caractere de ">", podendo ser um identificador para remoção caso a 1º linha
-# contenha.
-
-
-# Rodrigo - 15/07/2020
-
 
 def upload2(request):
     if request.method == 'POST':
@@ -100,6 +135,26 @@ def teste(request):
         print(arquivo)
         proteina = proteina.replace('\n','')
         print(len(proteina.replace(' ','')))
+
     return render(request, 'pipeline/teste.html')
     
 # Implementar uma Rotina para tentar pegar toda a sequencia inserida e começar a execução do Pipeline
+
+
+
+# A Fazeres: 
+
+#  Observações sobre esse tipo de implementação:
+#  Aqui eu consegui gerar uma função que recebe o arquivo enviado por 'POST' e pegar o conteúdo do arquivo enviado! 
+#  No entando eu precisei utilizar um formulário (preciso verificar e adaptar melhor o uso do mesmo) UploadFileForm.
+#  Ainda não está da forma que eu quero, mas já é um bom esboço para implementar a ideia do projeto.
+#  É importante lembrar que apesar de conseguir ler o conteúdo, existem vários pequenos problemas, como os caracteres "\r" e "\n" (pesquisar para que serve "\r")
+#  PS: optei por não armazenar os arquivos enviados por upload em um diretório (por enquanto), somente para evitar lixo eletrônico.
+
+# Agora preciso trabalhar melhor com os valores obtidos através de um arquivo disparado direto da página, por "padrão", vou tentar considerar
+# somente arquivos do tipo ".fasta", facilitando o trabalho. 
+# Preciso ignorar o header (1º Linha do arquivo), o mesmo começa com um caractere de ">", podendo ser um identificador para remoção caso a 1º linha
+# contenha.
+
+
+# Rodrigo - 15/07/2020
