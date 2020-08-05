bd85ce9e6d315b10bb4e3403afa4b07163b6a5c2
Rodrigo Santiago <33rodrigosantiago@gmail.com>
2020-08-05 17:18:21 -0300

    Nessa Versão do código eu aprendi a usar o Ajax junto ao Jquery para requisições assincronas, aprendi também a utilizar o Json para o retorno apenas dos dados necessários, agora vou usar essa tecnologia para trabalhar melhor junto ao Django!

--- BetaTCC/funcoes/views.py
+++ BetaTCC/funcoes/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from .forms import *
+from django.http import JsonResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'funcoes/index.html') 
+    
+    if request.method == 'POST':
+        nome = 'Rodrigo Santiago Claro Filho'
+        return nome
+        
 
+    
 
 def Index(request):
     return render(request, 'funcoes/index.html') 
@@ -17,6 +23,19 @@ def comparar(request):
     dados = {}
 
     if request.method == 'POST':
+
+        if(request.POST.get('act') == 'teste'):
+            dados = {
+                'seq1': request.POST.get('sequencia1'),
+                'seq2': request.POST.get('sequencia2'),
+            }
+            # return render(request, "funcoes/comparar.html", 
+            # {
+            #     'dados': dados 
+            # })
+            
+            return JsonResponse(dados)
+
         dados = {
            'nome1': request.POST.get('nome1'),
            'nome2': request.POST.get('nome2'),
@@ -37,16 +56,20 @@ def comparar(request):
         x = 0
         resultado = []
         if len(sequencia1) > len(sequencia2):
-            maior = len(sequencia1)
+            maior = len(sequencia1.replace("\n",""))
             for idx in range(len(sequencia2)):
+                if (sequencia2[idx] == '\n'):
+                    continue
                 if sequencia1[idx] == sequencia2[x]:
                     resultado.append('M') # Match
                 else:
                     resultado.append('D') # Diferente
                 x+=1
         else:
-            maior = len(sequencia2)
+            maior = len(sequencia2.replace("\n",""))
             for idx in range(len(sequencia1)):
+                if (sequencia1[idx] == '\n'):
+                    continue
                 if sequencia2[idx] == sequencia1[x]:
                     resultado.append('M') # Match
                 else:
@@ -54,7 +77,7 @@ def comparar(request):
                 x+=1
 
         #print(resultado)
-        #print(x)
+        print(maior)
 
         while x != maior:
             resultado.append('D')
@@ -73,14 +96,75 @@ def comparar(request):
 
     return render(request, "funcoes/comparar.html")
 
+# Backup
+
+# def comparar(request):
+    
+#     dados = {}
+
+#     if request.method == 'POST':
+#         dados = {
+#            'nome1': request.POST.get('nome1'),
+#            'nome2': request.POST.get('nome2'),
+#            'sequencia1': request.POST.get('sequencia1'),
+#            'sequencia2': request.POST.get('sequencia2'),
+#         }
+
+#         sequencia1 = request.POST.get('sequencia1')
+#         sequencia2 = request.POST.get('sequencia2')
+
+#         #print(len(sequencia1))
+#         #print(len(sequencia2))
+
+#         #print(sequencia1[1])
+
+#         # Aqui eu identifico qual é menor e realizo a iteração em cima dela
+#         # Pois os valores após o seu tamanho serão automaticamente "Diferentes"
+#         x = 0
+#         resultado = []
+#         if len(sequencia1) > len(sequencia2):
+#             maior = len(sequencia1.replace("\n",""))
+#             for idx in range(len(sequencia2)):
+#                 if (sequencia2[idx] == '\n'):
+#                     continue
+#                 if sequencia1[idx] == sequencia2[x]:
+#                     resultado.append('M') # Match
+#                 else:
+#                     resultado.append('D') # Diferente
+#                 x+=1
+#         else:
+#             maior = len(sequencia2.replace("\n",""))
+#             for idx in range(len(sequencia1)):
+#                 if (sequencia1[idx] == '\n'):
+#                     continue
+#                 if sequencia2[idx] == sequencia1[x]:
+#                     resultado.append('M') # Match
+#                 else:
+#                     resultado.append('D') # Diferente
+#                 x+=1
+
+#         #print(resultado)
+#         print(maior)
+
+#         while x != maior:
+#             resultado.append('D')
+#             x +=1
         
 
-    
+#         #print(x)
+#         #print(resultado)
 
 
-    # else:
+#         return render(request, "funcoes/comparar.html", 
+#         {
+#             'dados': dados, 
+#             'resultado': resultado
+#         })
 
-    #     return render(request, 'funcoes/comparar.html') 
+#     return render(request, "funcoes/comparar.html")
+
+
+
 
 
 def formulario(request):
